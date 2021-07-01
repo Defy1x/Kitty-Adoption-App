7177d4439334bc74b6d4ebe55e92f7417cc2fb14
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-07-01 17:25:18 -0400

    installing dependancies

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const axios = require('axios');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require("./config/connection.js");
+const helpers  = require('./utils/helpers.js');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const app = express();
+
+app.use(session(sessionOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+app.use(errorHandler);
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+});
