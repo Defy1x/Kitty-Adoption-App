e26f832d09d69fb1fee9e4fe5d9f306c6363b4c6
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-07-01 20:12:51 -0400

    added seeds

---
+++ seeds/kittyData.js
@@ -0,0 +1,51 @@
+const sequelize = require("../config/connection.js");
+const { Kitty } = require("../models");
+
+const kittyData = [
+    {
+        userId: 1,
+        kittyName: "King Henry",
+        kittyStory: "This cat thinks he is a king.",
+        kittyPicture: "https://media.rawg.io/media/games/3ea/3ea3c9bbd940b6cb7f2139e42d3d443f.jpg",
+        kittyBreed: "Scottish Fold",
+        kittyWeight: "Slim",
+        kittyColor: "White",
+        kittyLocation: "New York",
+        kittyPersonality: "Playful",
+        kittyAge: 3,
+        kittyKids: true,
+        kittyPets: false,
+    },
+    {
+      userId: 1,
+      kittyName: "Felix",
+      kittyStory: "Felix is a shy guy looking for a new home, he is good with kids and other animals.",
+      kittyPicture: "https://media.rawg.io/media/games/3ea/3ea3c9bbd940b6cb7f2139e42d3d443f.jpg",
+      kittyBreed: "Short Hair",
+      kittyWeight: "Average",
+      kittyColor: "Black",
+      kittyLocation: "New York",
+      kittyPersonality: "Shy",
+      kittyAge: 6,
+      kittyKids: true,
+      kittyPets: true,
+    },
+    {
+      userId: 1,
+      kittyName: "Wesker",
+      kittyStory: "Wesker secretly wants to kill you at night and steal all your cash. Hide your wife too.",
+      kittyPicture: "https://media.rawg.io/media/games/3ea/3ea3c9bbd940b6cb7f2139e42d3d443f.jpg",
+      kittyBreed: "Unknown",
+      kittyWeight: "Chonky",
+      kittyColor: "White / Brown",
+      kittyLocation: "New York",
+      kittyPersonality: "Asshole",
+      kittyAge: 7,
+      kittyKids: false,
+      kittyPets: false,
+    }
+]
+
+const seedKitty = () => Kitty.bulkCreate(kittyData);
+
+module.exports = seedKitty;
