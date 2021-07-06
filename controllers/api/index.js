df62a15bc8ca25bc31b065500922cf692806f09d
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-07-06 11:18:32 -0400

    changes to index in routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const kittyRoutes = require("./kitty-routes");
 const userRoutes = require("./user-routes");
 
-
 router.use("/user", userRoutes);
 router.use("/kitty", kittyRoutes);
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,28 @@
-const router = require("express").Router();
-const checkAuthorization = require('../utils/authorization');
-
-const apiRoutes = require("./api");
-const homeRoutes = require("./home-routes");
-const kittyRoutes = require("./kitty-routes");
-
-router.use("/", homeRoutes);
-router.use("/kitty", kittyRoutes);
-router.use("/api", apiRoutes);
-
-module.exports = router;
-
-
-// const path = require("path");
 // const router = require("express").Router();
-// const apiRoutes = require("./api");
 // const checkAuthorization = require('../utils/authorization');
 //
-// // API Routes
+// const apiRoutes = require("./api");
+// const homeRoutes = require("./home-routes");
+// const kittyRoutes = require("./kitty-routes");
+//
+// router.use("/", homeRoutes);
+// router.use("/kitty", kittyRoutes);
 // router.use("/api", apiRoutes);
 //
-// // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-//
 // module.exports = router;
+
+
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const checkAuthorization = require('../utils/authorization');
+
+// API Routes
+router.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
