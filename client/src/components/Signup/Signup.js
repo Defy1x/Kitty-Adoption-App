ccddca57e1bb4cb9510edd9a7a104b71a7e6e6d7
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-07-12 00:23:26 -0400

    two pages done, seven left

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -13,18 +13,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Zen Cat
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -119,16 +107,11 @@ export default function SignUp() {
           </Button>
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
+            <Link to='/signup'><p className="SignupLink">Don't have an account? Sign up.</p></Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
