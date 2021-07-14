2ad14f0dc3547c9e6dc79569104e8339b1d59b24
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-07-13 22:35:46 -0400

    testing

--- server.js
+++ server.js
@@ -30,7 +30,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
+app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.use(express.json());
